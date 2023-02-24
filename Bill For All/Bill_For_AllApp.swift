1cf51e9545f11c8ea33e338b5ed256c8c711ad7f
Javier Gonzalez <doniagv@gmail.com>
2023-02-24 12:29:14 -0600

    Initial Commit

---
+++ Bill For All/Bill_For_AllApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bill_For_AllApp.swift
+//  Bill For All
+//
+//  Created by Javier Andoni González Vázquez on 24/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Bill_For_AllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
